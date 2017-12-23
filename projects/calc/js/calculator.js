dace7054905e2beff59d154f463eafd3e51fa166
May <may.schiller@gmail.com>
2017-12-23 21:57:27 +0200

    may

--- projects/calc/js/calculator.js
+++ projects/calc/js/calculator.js
@@ -1,153 +1,109 @@
+'use strict'
 
-var gNum1 = 0;;
-var gNum2 = 0;
-var gMemoryNum = 0;
-var gOp;
+var gNum1 = '';
+var gNum2 = null;
+var gOper;
+var gResult;
+var gElScreen = document.querySelector('.screen');
+var gMemoryNum;
 
-function addDigit(elNum) {
 
-    var num = +getCellNum(elNum.id);
-    if (!gNum1) {
-        gNum1 += num;
-        gNum2 += gNum1;
-        var elScreen = document.querySelector('#screen');
-        elScreen.innerHTML = gNum2;
-    }
-    else {
-        gNum2 = gNum2 * 10 + num;
-        var elScreen = document.querySelector('#screen');
-        elScreen.innerHTML = gNum2;
+function addDigit(digit) {  
+    if (gNum2 === null) {
+        gNum1 += digit      
+        gElScreen.innerHTML = gNum1;
+    } else {
+       gNum2 += digit; 
+       gElScreen.innerHTML = gNum2;
     }
 }
 
-function equal() {
 
-    var elEqual = document.querySelector('#screen');
-    elEqual.innerHTML = eval(gNum2);
+function arithmetic(oper) {
+    switch (oper) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            gNum2 = '';
+            gOper = oper;
+            gElScreen.innerHTML = gOper;
+            break;
+        
+        case '=':
+            if (gOper === '+') { 
+                gResult = (+gNum1) + (+gNum2);      // easier to read with ();
+            } else if (gOper === '-') {
+                gResult = (+gNum1) - (+gNum2);
+            } else if (gOper === '*') {
+                gResult = (+gNum1) * (+gNum2);
+            } else if (gOper === '/') {
+                gResult = (+gNum1) / (+gNum2);
+            }
+            
+            gElScreen.innerHTML = gResult; 
+            gNum1 = gResult;      
+    }
 }
 
-function sum() {
 
-    gOp = '+';
-    gNum2 += gOp;
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
+function del() {
+   
+    if (gNum2 === null) {
+        gNum1 = gNum1.slice(0, -1);
+        gElScreen.innerHTML = gNum1;
+    } else if (gResult) {     
+        return;
+    } else {
+        gNum2 = gNum2.slice(0, -1);
+        gElScreen.innerHTML = gNum2;
+    }
 }
-
-function pow(){
-
-    gOp = '**';
-    gNum2 += gOp;
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
-}
-
-function root(){
-
-    // gOp = '** 1/';
-    // gNum2 += gOp;
-
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = Math.sqrt(gNum2);
-    gNum1 = '';
-}
-
-function div() {
-
-    gOp = '/';
-    gNum2 += gOp;
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
-}
-
-function subs() {
-
-    gOp = '-';
-    gNum2 += gOp;
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
-}
-
-function mult() {
-
-    gOp = '*';
-    gNum2 += gOp;
-    elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
-}
-
-function getCellNum(elNum) {
-
-    var arrId = elNum.split('-');
-    var num = arrId[1];
-    return num;
-}
-
 
 function clearNum() {
-    gNum1 = null;
-    gNum2 = null;
-    var elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = 0;
+    if (gNum2 === null) {
+        gNum1 = '';
+        gElScreen.innerHTML = gNum1;
+    } else if (gResult) {
+        gNum1 = '';
+        gNum2 = null;
+        gResult = '';
+        gElScreen.innerHTML = gResult;
+    } else {
+        gNum2 = '';
+        gElScreen.innerHTML = gNum2;
+    }
 }
 
-function addMemory(elButton) {
-    // console.log('gNum2',eval(gNum2))
-    gMemoryNum += eval(gNum2);
-}
-
-function removeMemory(elButton) {
-    gMemoryNum -= eval(gNum2);
-
-}
-
-function clearMemory(elButton) {
-    gMemoryNum = 0;
-}
-
-function pullData(elButton) {
-    document.querySelector('#screen').innerHTML = gMemoryNum;
-}
-
-
-function hex() {
-    console.log('hi')
-
-    var res = parseInt(eval(gNum2), 10).toString(16);
-    document.querySelector('#screen').innerHTML = res.toLocaleUpperCase();
-
-}
-function dec() {
-    console.log('hii')
-    document.querySelector('#screen').innerHTML = eval(gNum2);
-}
-function oct() {
-    document.querySelector('#screen').innerHTML = parseInt(eval(gNum2), 10).toString(8);
-}
-function bin() {
-
-    document.querySelector('#screen').innerHTML = parseInt(eval(gNum2), 10).toString(2);
+function addToMemory(action) {
+    switch (action) {
+        case 'mc':
+            gMemoryNum = 0;
+            break;
+        
+        case 'mr':
+            if (gNum2 === null) {
+                gNum1 = +gMemoryNum;
+                gElScreen.innerHTML = gNum1;
+            } else {
+                gNum2 = +gMemoryNum;;
+                gElScreen.innerHTML = gNum2;                
+            }
+            break;
+        
+        case 'ms':
+            gMemoryNum = +gElScreen.innerHTML;
+            break;                      
+        
+        case 'mp':
+            gMemoryNum = ((+gElScreen.innerHTML) + (+gMemoryNum));
+            break;
+        
+        case 'mm':
+            gMemoryNum = ((+gMemoryNum) - (+gElScreen.innerHTML));
+            break;
+    }
 }
 
 
-function deleteNum() {
 
-    var n = gNum2.toString();    
-    console.log('n',n)
-    gNum2 = n.substring(0, n.length - 1);
-    var elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2
-}
-
-function decP(){
-
-    gNum2+='.';
-    var elScreen = document.querySelector('#screen');
-    elScreen.innerHTML = gNum2;
-    gNum1 = '';
-}
